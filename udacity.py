24e9a3929359d33de42775b27e2aef89922ecbfb
Thibaut <thibaut.smith@telecomnancy.net>
2014-03-10 17:34:24 +0100

    (PS3) Fix: form.

--- udacity.py
+++ udacity.py
@@ -1,6 +1,7 @@
 import webapp2
 import os
 import jinja2
+import google.appengine.ext
 
 jinja_environment = jinja2.Environment(autoescape=True,
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
@@ -9,15 +10,33 @@ jinja_environment = jinja2.Environment(autoescape=True,
 class SubmitPage(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('submit_form.Jinja2')
-        self.response.out.write(template.render(template_values))
+        values = {
+                'subject':"",
+                'content':"",
+                'error':""
+        }
+        self.response.out.write(template.render(values))
 
     def post(self):
-        form = web.input(subject, content)
+        values = {
+                'subject':"",
+                'content':"",
+                'error':""
+        }
 
-        self.reponse.out.write("Coucou")
+        values['subject'] = self.request.get("subject")
+        values['content'] = self.request.get("content")
+
+        if values['subject'] and values['content']:
+            pass
+        else:
+            values['error'] = "Please enter all fields, noob."
+
+        template = jinja_environment.get_template('submit_form.Jinja2')
+        self.response.out.write(template.render(values))
+
 
 application = webapp2.WSGIApplication([
     ('/blog/newpost', SubmitPage),
-
 ], debug=True)
 
